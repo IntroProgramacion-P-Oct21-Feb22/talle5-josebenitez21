28c22d874caea675a0f5ceb719f5d99c804a0ca7
Jose Benitez <jlbenitez4@utpl.edu.ec>
2021-11-12 12:02:09 -0500

    primer commit

--- problema1/problema1/src/problema1/Problema1.java
+++ problema1/problema1/src/problema1/Problema1.java
@@ -19,53 +19,34 @@ public class Problema1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+
         Scanner entrada = new Scanner(System.in);
         entrada.useLocale(Locale.US);
-        
+
         double costoKilovatio;
         double numeroKilovatios = 0;
         double valorTotal;
         int edad = 0;
         int porcentaje = 10;
         double descuento;
-        
+
         System.out.println("Ingrese el valor de costo por kilovatio");
         costoKilovatio = entrada.nextDouble();
-        
+
         System.out.println("Ingrese el numero de kilovatios consumidos en el mes");
         costoKilovatio = entrada.nextDouble();
-        
+
         valorTotal = (costoKilovatio * numeroKilovatios);
-        if (edad>=65){
-            descuento = (porcentaje * valorTotal)/100;
-            valorTotal = valorTotal-descuento;
+        if (edad >= 65) {
+            descuento = (porcentaje * valorTotal) / 100;
+            valorTotal = valorTotal - descuento;
+
             
-        System.out.printf("el valor a pagar es: %.2f",valorTotal);
-            
-                   
+
         }
+        
+        System.out.printf("el valor a pagar es: %.2f", valorTotal);
 
- 
-        
-       
-        
-        
-        
-        
-        
-                
-       
-        
-        
-        
-        
-        
     }
-    
+
 }
-
-
-
-
-
